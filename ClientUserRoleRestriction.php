a8d140e081ad31c6d9d779fe252f9fc633698f99
btrebach <ben.trebach@gmail.com>
2014-04-12 14:08:04 -0400

    added role restriction

---
+++ ClientUserRoleRestriction.php
@@ -0,0 +1,14 @@
+/* Restrict access for client user role */ 
+
+function restrict_access_admin_panel()
+{
+	global $current_user;
+	get_currentuserinfo();
+	if ($current_user->user_level < 4)
+	{
+	wp_redirect( get_bloginfo('url') );
+	exit;
+	}
+}
+
+add_action('admin_init', 'restrict_access_admin_panel', 1);
