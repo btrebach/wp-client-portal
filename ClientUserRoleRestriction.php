1f1849eb560556c66cb49e249f65262b9dff9e4c
btrebach <ben.trebach@gmail.com>
2014-04-13 11:59:33 -0400

    corrected role, fixed issue #1

--- ClientUserRoleRestriction.php
+++ ClientUserRoleRestriction.php
@@ -4,7 +4,7 @@ function restrict_access_admin_panel()
 {
 	global $current_user;
 	get_currentuserinfo();
-	if ($current_user->user_level < 4)
+	if ($current_user->user_level < 5)
 	{
 	wp_redirect( get_bloginfo('url') );
 	exit;
