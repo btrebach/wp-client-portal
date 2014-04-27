5f27bb03a7e2a9eb0c4742723953da69c7a5b327
btrebach <ben.trebach@gmail.com>
2014-04-27 16:43:34 -0400

    commented

--- FileUploader.php
+++ FileUploader.php
@@ -18,14 +18,6 @@ function fileupload( $label ) { ?>
   </tr>  <?php
 }
 
-/**
- * Handle file uploads
- *
- * @todo check nonces
- * @todo check file size
- *
- * @return none
- */
 function fileupload_process() { 
   $uploadfiles = $_FILES['uploadfiles'];
 
@@ -33,7 +25,7 @@ function fileupload_process() {
 
     foreach ($uploadfiles['name'] as $key => $value) {
 
-      // look only for uploded files
+      //look only for uploaded files
       if ($uploadfiles['error'][$key] == 0) {
 
         $filetmp = $uploadfiles['tmp_name'][$key];
@@ -41,8 +33,7 @@ function fileupload_process() {
         //clean filename and extract extension
         $filename = $uploadfiles['name'][$key];
 
-        // get file info
-        // @fixme: wp checks the file extension....
+        //get file info
         $filetype = wp_check_filetype( basename( $filename ), null );
         $filetitle = preg_replace('/\.[^.]+$/', '', basename( $filename ) );
         $filename = $filetitle . '.' . $filetype['ext'];
@@ -59,17 +50,15 @@ function fileupload_process() {
         }
         $filedest = $upload_dir['path'] . '/' . $filename;
 
-        /**
-         * Check write permissions
-         */
+        
+        //Check write permissions 
         if ( !is_writeable( $upload_dir['path'] ) ) {
           $this->msg_e('Unable to write to directory %s. Is this directory writable by the server?');
           return;
         }
 
-        /**
-         * Save temporary file to uploads dir
-         */
+        
+        //Save temporary file to uploads directory 
         if ( !@move_uploaded_file($filetmp, $filedest) ){
           $this->msg_e("Error, the file $filetmp could not moved to : $filedest ");
           continue;
